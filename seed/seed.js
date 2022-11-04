852111fddc0f8ac5e0e704bbc0816de59d843366
Alexis Herrera <alexisherrera@Alexiss-MacBook-Pro.local>
2022-11-04 16:22:22 -0400

    errors trying to link collections by transactions

--- seed/seed.js
+++ seed/seed.js
@@ -1,13 +1,38 @@
-/*
+import db from "../db/connection.js";
+import Transaction from "../models/Transaction.js"
+import UserProfile from "../models/UserProfile.js"
+import profileData from "./profileData.json" assert {type: "json"};
+import transactionData from "./transactionData.json" assert {type: "json"};
 
-let updatedProfile = UserProfile.map((user) => {
-  Transaction.forEach((transaction) => {
-    if (user.email === (transaction.to || transaction.from)) {
-      user.transactions.push(transaction)
-    }
+
+const updateProfile = async () => {
+
+  let profiles = UserProfile.find().toArray()
+  let transactions = Transaction.find().toArray()
+
+  await profiles.forEach((user) => {
+      transactions.forEach((transaction) => {
+        if (user.email === (transaction.to || transaction.from)) {
+          let thisUser = UserProfile.find({ _id: user._id })
+          let thisTransaction = Transaction.find({ _id: transaction._id})
+        thisUser.transactions.push(thisTransaction)
+        thisUser.save()
+      }
+    })
   })
-  return user
-})
+}
 
+const createDatabase = async () => {
+  
 
-*/
+  await db.dropDatabase();
+  await UserProfile.create(profileData)
+  await Transaction.create(transactionData)
+
+  await updateProfile()
+
+  // await UserProfile.create(updatedProfile)
+  await db.close()
+}
+
+createDatabase();
