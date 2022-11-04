e397aaf991d335b1c0248bcd29a2ee4ff8b92be4
Alexis Herrera <alexisherrera@Alexiss-MacBook-Pro.local>
2022-11-04 11:06:47 -0400

    created user and transaction models

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,14 @@
-//transaction model
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+let Transaction = new Schema({
+
+  to: { type: String },
+  from: { type: String }, 
+  currency: { type: String },
+  amount: { type: Number },
+  sentOn: {type: Date}
+
+})
+
+export default mongoose.model("Transactions", Transaction)
--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,2 +1,15 @@
-//UserProfile model
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+import Transaction from "./Transaction.js"
 
+let UserProfile = new Schema({
+
+  email: { type: String },
+  firstName: { type: String },
+  lastName: { type: String },
+  password: { type: String },
+  avatarImg: { type: String },
+  transaction: [Transaction]
+})
+
+export default mongoose.model("UserProfiles", UserProfile)
