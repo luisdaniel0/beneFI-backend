d4d59707463ce61b358e7481b0a4613f06602b46
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-Air.fios-router.home>
2022-11-06 12:09:08 -0500

    trying to link collections

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ const router = Router();
 router.get("/", (req, res) => res.send("This is the api root!"));
 
 router.use("/transaction", transactionRoutes);
-router.use("/UserProfile", userProfileRoutes);
+router.use("/userProfile", userProfileRoutes);
 
 export default router;
