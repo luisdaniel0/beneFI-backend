d097363e07393948f71aa6cfa112a39c8c602198
Luis Sanchez <luisanchezdh@gmail.com>
2023-02-08 13:03:25 -0500

    mongodb

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ import chalk from "chalk";
 mongoose.set("returnOriginal", false);
 
 mongoose
-  .connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/beneFi-user-data")
+  .connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/benefi-backend')
   .catch((err) => {
     console.log(`Error connection go MongoDB: ${err.message}`);
   });
